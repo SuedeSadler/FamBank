8999547875cd5db67ea1f178380087ccaf98dd71
SuedeSadler <Suede.lang.sadler@gmail.com>
2024-08-26 16:07:55 +1200

    Crsipy forms added

--- GroupSavingsAdmin/GroupSavingsAdmin/settings.py
+++ GroupSavingsAdmin/GroupSavingsAdmin/settings.py
@@ -49,7 +49,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- GroupSavingsAdmin/savings/forms.py
+++ GroupSavingsAdmin/savings/forms.py
@@ -3,18 +3,48 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Group
 from .models import Invitation
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, Row, Column, Field
+
 
 class InvitationForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(InvitationForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Field('user', css_class='form-control')
+        )
+        self.helper.add_input(Submit('submit', 'Send Invitation', css_class='btn btn-primary'))
+
     class Meta:
         model = Invitation
-        fields = ['user'] 
+        fields = ['user']
 
 class AddMemberForm(forms.Form):
     user = forms.ModelChoiceField(queryset=User.objects.all(), label="Select User to Add")
 
+    def __init__(self, *args, **kwargs):
+        super(AddMemberForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Field('user', css_class='form-control')
+        )
+        self.helper.add_input(Submit('submit', 'Add Member', css_class='btn btn-primary'))
+
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
+
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Field('username', css_class='form-control'),
+            Field('email', css_class='form-control'),
+            Field('password1', css_class='form-control'),
+            Field('password2', css_class='form-control'),
+        )
+        self.helper.add_input(Submit('submit', 'Register', css_class='btn btn-primary'))
 
     class Meta:
         model = User
@@ -27,7 +57,18 @@ class CustomUserCreationForm(UserCreationForm):
             user.save()
         return user
 
+
 class GroupCreationForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(GroupCreationForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Field('name', css_class='form-control'),
+            Field('description', css_class='form-control'),
+        )
+        self.helper.add_input(Submit('submit', 'Create Group', css_class='btn btn-primary'))
+
     class Meta:
         model = Group
         fields = ['name', 'description']
+
