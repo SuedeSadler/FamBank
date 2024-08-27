83b104fcd4de9456e5c087a045f7466f091b8a2a
Suede Sadler <Suede.lang.sadler@gmail.com>
2024-08-27 22:49:10 +1200

    Updated dir

--- GroupSavingsAdmin/settings.py
+++ GroupSavingsAdmin/settings.py
@@ -37,6 +37,9 @@ CSRF_TRUSTED_ORIGINS = [
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'savings', 'static'),
+]
 
 # Media files (uploads)
 MEDIA_URL = '/media/'
@@ -72,7 +75,7 @@ ROOT_URLCONF = 'GroupSavingsAdmin.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'savings', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
